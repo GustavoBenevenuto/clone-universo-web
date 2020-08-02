4b1a9da5628b875c91dc2b92128edf467e7d5948
Gustavo Benevenuto <gustavo.benesilva@gmail.com>
2020-08-02 00:53:36 -0300

    Conclusão do dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Conteudo, Card } from './styles';
+import { Conteudo, Card, AlertCard } from './styles';
 import LayoutHeader from '../../components/LayoutHeader';
 import LayoutMenu from '../../components/LayoutMenu';
 
 import barCode from '../../img/card/barcode.png'
+import professional from '../../img/card/professional.png'
+import virus from '../../img/card/virus.png'
+import send from '../../img/send.png'
 
 const Dashboard: React.FC = () => {
 
@@ -16,7 +19,7 @@ const Dashboard: React.FC = () => {
                     <div className="conteudo-top">
                         <h1>Principal</h1>
                         <div className="conteudo-card">
-                            <Card>
+                            <Card cor="#2172FF">
                                 <h2>Boleto Online</h2>
                                 <div>
                                     <p>R$ 450,51</p>
@@ -24,18 +27,77 @@ const Dashboard: React.FC = () => {
                                 </div>
                             </Card>
 
-                            <Card>
-                                <h2>Boleto Online</h2>
+                            <Card cor="#711899">
+                                <h2>Secretaria Online</h2>
                                 <div>
-                                    <p>R$ 450,51</p>
-                                    <img src={barCode} alt="" />
+                                    <p>Protocolos Abertos: 6</p>
+                                    <img src={professional} alt="" />
+                                </div>
+                            </Card>
+
+                            <Card cor="#283034">
+                                <h2>COVID-19</h2>
+                                <div>
+                                    <p>Recomendações</p>
+                                    <img src={virus} alt="" />
                                 </div>
                             </Card>
                         </div>
                     </div>
 
                     <div className="conteudo-bottom">
+                        <h1>Alertas</h1>
+                        <AlertCard>
+                            <header>
+                                <h3>Alteração na data das provas</h3>
+                                <label>21/02/2020</label>
+                            </header>
+                            
+                            <label>Descrição</label>
+                            
+                            <section>
+                                <p>
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem numquam inventore iusto asperiores! Voluptas dolore iste nesciunt hic nulla nam ea nostrum? Distinctio optio dicta eum velit atque nisi amet.
+                                </p>
+                                
+                                <span>Veja <img src={send} alt=""/></span>
+                            </section>
+                        </AlertCard>
 
+                        <AlertCard>
+                            <header>
+                                <h3>Novas informações sobre a COVID-19</h3>
+                                <label>21/02/2020</label>
+                            </header>
+                            
+                            <label>Descrição</label>
+                            
+                            <section>
+                                <p>
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem numquam inventore iusto asperiores! Voluptas dolore iste nesciunt hic nulla nam ea nostrum? Distinctio optio dicta eum velit atque nisi amet.
+                                </p>
+                                
+                                <span>Veja <img src={send} alt=""/></span>
+                            </section>
+                        </AlertCard>
+                    
+                        <AlertCard>
+                            <header>
+                                <h3>Renovação da matrícula em breve</h3>
+                                <label>21/02/2020</label>
+                            </header>
+                            
+                            <label>Descrição</label>
+                            
+                            <section>
+                                <p>
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem numquam inventore iusto asperiores! Voluptas dolore iste nesciunt hic nulla nam ea nostrum? Distinctio optio dicta eum velit atque nisi amet.
+                                </p>
+                                
+                                <span>Veja <img src={send} alt=""/></span>
+                            </section>
+                        </AlertCard>
+                    
                     </div>
                 </main>
             </Conteudo>
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
+interface CardProps {
+    cor: string;
+}
 
 export const Conteudo = styled.div`
     display: flex;
@@ -9,17 +12,17 @@ export const Conteudo = styled.div`
         display: flex;
         flex-direction: column;
         
+        h1{ 
+            font-size: 35px;
+        }
+
         .conteudo-top{
             display: flex;
             flex-direction: column;
             flex: 1.3;
             border-bottom: 2px solid #CCC;
             margin-top: 20px;
-
-            h1{ 
-                font-size: 35px;
-                margin-left: 35px;
-            }
+            padding-left: 35px;
 
             .conteudo-card {
                 display: flex;
@@ -30,18 +33,75 @@ export const Conteudo = styled.div`
         .conteudo-bottom{
             display: flex;
             flex: 3;
+            flex-direction: column;
+            margin-top: 20px;
+            padding: 0px 35px;
         }
     }
 `;
 
-export const Card = styled.div`
+export const AlertCard = styled.a`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 150px;
+    border: 1px solid #CCC;
+    border-radius: 15px;
+    padding: 10px;
+    margin-top: 20px;
+
+    h3 {
+        font-size: 23px;
+    }
+
+    label {
+        color: #AAA;
+    }
+
+    img{
+        width: 25px;
+        margin-left: 5px
+    }
+
+    header{
+        display: flex;
+        justify-content: space-between;
+    }
+
+    section {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        p {
+            width: 1000px;
+        }
+
+        span {
+            display: flex;
+            color: #2172FF;
+        
+            &:hover {
+                cursor: pointer;
+                opacity: 0.7;
+            }
+        }
+    }
+`;
+
+export const Card = styled.a<CardProps>`
     height: 150px;
     width: 350px;
     border-radius: 12px;
-    margin-left: 35px;
-    background-color: #2172FF;
+    background-color: ${props =>  props.cor ? props.cor : '#2172FF'};
     color: #FFF;
     padding : 0px 25px;
+    margin-right: 35px;
+    
+    &:hover{
+        cursor: pointer;
+        opacity: 0.9;
+    }
 
     h2{
         margin: 20px 0px;
